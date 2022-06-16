4abda4216262a638276495fe4d9fa3c7fd9a86df
Rachael-Minage <lauryneminagge@gmail.com>
2022-06-16 16:57:51 +0300

    first commit

---
+++ practice_one.py
@@ -0,0 +1,9 @@
+def reverse_number(number):
+    rev = 0
+    while number !=0:
+        rev = (rev*10)+(number%10)
+        number = number//10
+
+    return rev,number
+
+print(reverse_number(98765))
